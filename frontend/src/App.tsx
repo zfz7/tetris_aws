ab10cf097f60c2c028bd5038a2a380a836117e31
Daniel Eichman <d.eichman@outlook.com>
2023-08-11 13:19:05 -0700

    Use updated cognito pattern

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,59 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import {SayHelloInput, SayHelloOutput} from 'ts-client'
 import {Amplify} from 'aws-amplify';
 import {Authenticator} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import {getHello, getInfo} from "./utils/client";
+import {AmplifyUser, AuthEventData} from '@aws-amplify/ui';
 
-Amplify.configure({
-    Auth: {
-        region: "us-west-2",
-        userPoolId: "us-west-2_Z6q0XxKpE", //TODO do not hard code this
-        userPoolWebClientId: "28nea2k4nbcjnameihjju3d0g", //TODO do not hard code this
-        authenticationFlowType: "USER_PASSWORD_AUTH",
-    }
-});
 
 function App() {
-    const [input, setInput] = useState("Enter input:");
-    const [output, setOutput] = useState("");
+    const [showLoginScreen, setShowLoginScreen] = useState(false);
 
-    return (
+    useEffect(() => {
+        getInfo().then(config => {
+            Amplify.configure({
+                Auth: {
+                    ...config
+                }
+            });
+        })
+    }, [])
+
+    interface HomeScreenProps {
+        user: AmplifyUser | undefined,
+        signOut: ((data?: AuthEventData | undefined) => void) | undefined
+    }
+
+    const HomeScreen = ({user, signOut}: HomeScreenProps) => {
+        const [input, setInput] = useState("");
+        const [output, setOutput] = useState("");
+        return (<div
+            className="App-header">
+            <h1>Tetris template</h1>
+            <h4>Hello {user?.attributes!["given_name"]} {user?.attributes!["family_name"]}</h4>
+            {signOut ?
+                <button onClick={signOut}>Sign out</button>
+                :
+                <button onClick={() => setShowLoginScreen(true)}>Sign in</button>
+            }
+            <div>Send to server:</div>
+            <input onChange={(event) => setInput(event.target.value)}></input>
+            <br/>
+            <button onClick={() =>
+                getHello({name: input}, user?.getSignInUserSession()?.getIdToken()?.getJwtToken()!).then(
+                    (res) => setOutput(res.message!),
+                    (err) => console.log(err)
+                )}>
+                click me
+            </button>
+            <div>Server response</div>
+            <div>{output}</div>
+        </div>)
+    }
+
+
+    return showLoginScreen ?
         <Authenticator
             variation="modal"
             loginMechanisms={['email']}
@@ -27,40 +62,9 @@ function App() {
                 'family_name',
                 'given_name',
             ]}>
-            {({signOut, user}) => (
-                <div className="App-header">
-                    <h1>Tetris template</h1>
-                    <h4>Hello {user?.attributes!["given_name"]} {user?.attributes!["family_name"]}</h4>
-                    <button onClick={signOut}>Sign out</button>
-                    <div>Send to server:</div>
-                    <input onChange={(event) => setInput(event.target.value)}></input>
-                    <br/>
-                    <button onClick={() =>
-                        sayHelloApi({name: input}, user?.getSignInUserSession()?.getIdToken()?.getJwtToken()!).then(
-                            (res) => setOutput(res.message!),
-                            (err) => console.log(err)
-                        )}>
-                        click me
-                    </button>
-                    <div>Server response</div>
-                    <div>{output}</div>
-                </div>
-            )}
+            {({signOut, user}) => <HomeScreen user={user} signOut={signOut}/>}
         </Authenticator>
-    );
-}
-
-const sayHelloApi = (input: SayHelloInput, bearerToken: string): Promise<SayHelloOutput> => {
-    const url = `https://api.${window.location.hostname}/hello`
-    return fetch(`${url}?` + new URLSearchParams({"name": input.name!}), {
-        method: 'GET',
-        headers: {
-            "Authorization": `Bearer ${bearerToken}`,
-        }
-    }).then(
-        (response) => response.json(),
-        (err) => console.log(err)
-    )
+        : <HomeScreen user={undefined} signOut={undefined}/>;
 }
 
 export default App;
